e4c2685bd4f8c24e5f3110334e8e53489f107992
polymorph-daniel <daniel@polymorph.email>
2022-11-20 11:22:05 -0600

    first commit

--- scrapingTest.py
+++ scrapingTest.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 page = requests.get("https://workflow-automation.podio.com/flows.php")
 page
 
-print(page.status_code)
+print(page.status_code)git
 
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
