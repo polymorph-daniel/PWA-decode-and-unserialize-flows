973c8aaeda303972777164ff60223227e8707728
polymorph-daniel <daniel@polymorph.email>
2022-11-21 08:38:54 -0600

    scraping test

--- scrapingTest.py
+++ scrapingTest.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pypodio2 import api
+from client_settings import *
+
+c = api.OAuthClient(
+    client_id,
+    client_secret,
+    username,
+    password,    
+)
+print c.Item.find(22342)
 
 page = requests.get("https://workflow-automation.podio.com/flows.php")
 page
 
-print(page.status_code)git
+print(page.status_code)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
